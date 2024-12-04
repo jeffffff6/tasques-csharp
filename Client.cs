48af76f3bae512cc8a2f21d9a307ba38e1a8b9a0
Youssef <lolbak006@gmail.com>
2024-12-04 03:50:04 +0100

    programa del banquer

---
+++ Client.cs
@@ -0,0 +1,53 @@
+namespace Banquer;
+
+class Client
+{
+    private string nom = "";
+    private int compte = 0;
+
+    public Client()
+    {
+        nom="";
+        compte=0;
+    }
+
+    public Client(string nom){
+        this.nom = nom;
+        compte = 0;
+    }
+    public Client(string nom, int saldoInicial){
+        this.nom = nom;
+        compte = saldoInicial;
+    }
+    public string Nom{
+        get{return nom;}
+    }
+    public int Compte{
+        get{return compte;}
+    }
+
+    public void posarDiners(){
+        Console.WriteLine("Quants diners vols afegir?");
+        int dinersASumar = int.Parse(Console.ReadLine());
+
+        if(dinersASumar>0){
+            compte+=dinersASumar;
+        }
+        else{
+            Console.WriteLine("No pots afegir un saldo negatiu!");
+        }
+    }
+    public void treureDiners(){
+        Console.WriteLine("Quants diners vols treure?");
+        int dinersARestar = int.Parse(Console.ReadLine());
+        if(compte>0+dinersARestar){
+            compte-=dinersARestar;
+        }
+        else{
+            Console.WriteLine("No tens suficient saldo!");
+        }
+    }
+    public void quantsDiners(){
+        Console.WriteLine(compte);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Banquer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        List<Client> clients = new List<Client>();
+        bool aplicacio = true;
+        Console.WriteLine("");
+        menu();
+        while(aplicacio){
+            
+            Console.WriteLine("Selecciona una opció del menú:");
+            char opcio = Console.ReadLine()?.FirstOrDefault() ?? '\0';
+            switch (opcio)
+            {
+                case 'a':
+                    CrearClient(ref clients);
+                    break;
+
+                case 'b':
+                    ConsultarClient(ref clients);
+                    break;
+
+                case 'c':
+                    //OperarClient(ref clients);
+                    break;
+
+                case 'd':
+                    aplicacio = false;
+                    Console.WriteLine("Cerrando aplicación...");
+                    break;
+                case 'm':
+                    menu();
+                    break;
+
+                default:
+                    Console.WriteLine("Opción no válida. Por favor, selecciona una opción del menú.");
+                    break;
+            }
+            Console.WriteLine("----------------------------------------");
+        }
+
+    }
+    static void menu(){
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("a. Crear client");
+        Console.WriteLine("b. Consultar client");
+        Console.WriteLine("c. Operar client");
+        Console.WriteLine("d. Tancar aplicacio");
+        Console.WriteLine("m. Mostrar menu");
+        Console.WriteLine("----------------------------------------");
+    }
+    static void CrearClient(ref List<Client> clients)
+    {
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("Escriu el teu nom: ");
+        var nom = Console.ReadLine();
+        while(nom == ""){
+            Console.WriteLine("El nom no pot estar en blanc. Introdueix un nom:");
+            nom=Console.ReadLine();
+        }
+        bool clientTrobat = false;
+        foreach(Client client in clients){
+            if(client.Nom == nom){
+                clientTrobat = true;
+                break;
+            }
+        }
+
+        if(!clientTrobat){
+            Client client = new Client(nom);
+            clients.Add(client);
+            Console.WriteLine("El client s'ha afegit correctament.");
+        }
+        else{
+            Console.WriteLine("Ja existeix un client amb aquest nom.");
+        }
+    }
+    static void ConsultarClient(ref List<Client> clients)
+    {
+        Console.WriteLine("----------------------------------------");
+        if(clients.Count == 0){
+            Console.WriteLine("No hi ha clients a consultar.");
+        }
+        else{
+            Console.WriteLine("Nom del client a consultar: ");
+            string nom = Console.ReadLine();
+            bool clientTrobat = false;
+
+            foreach(Client client in clients){
+                if(client.Nom == nom){
+                    clientTrobat = true;
+                    Console.WriteLine($"{client.Nom} tens aquest saldo a la teva compte: {client.Compte}");
+                    break;
+                }
+            }
+
+            if (!clientTrobat)
+            {
+                Console.WriteLine("El client no existeix.");
+            }
+        }
+    }
+}
